f27af1c53f87ccb6e44b7e4df5ae8df39d08ac1b
successtar <osanyinpejuhammed35@gmail.com>
2019-02-19 13:18:13 +0100

    Source code Files

---
+++ app/src/main/java/com/myexampoint/webtoapp/MainActivity.java
@@ -0,0 +1,106 @@
+package com.myexampoint.webtoapp;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    String ShowOrHideWebViewInitialUse = "show";
+    private WebView webview ;
+    private ProgressBar spinner;
+    String myurl = "https://myexampoint.com"; //Change this  to your website hostname
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview =(WebView)findViewById(R.id.webView);
+        spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        webview.setWebViewClient(new CustomWebViewClient());
+
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl(myurl);
+
+    }
+
+
+
+    // This allows for a splash screen
+    // (and hide elements once the page loads)
+    private class CustomWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+
+            // only make it invisible the FIRST time the app is run
+            if (ShowOrHideWebViewInitialUse.equals("show")) {
+                webview.setVisibility(webview.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            ShowOrHideWebViewInitialUse = "hide";
+            spinner.setVisibility(View.GONE);
+
+            view.setVisibility(webview.VISIBLE);
+            super.onPageFinished(view, url);
+
+        }
+
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode,
+                                    String description, String failingUrl) {
+            //   view.loadUrl("about:blank");
+            myurl = view.getUrl();
+            setContentView(R.layout.error);
+            super.onReceivedError(view, errorCode, description, failingUrl);
+        }
+
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webview.canGoBack()) {
+                        webview.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    public void bottomClick(View v){
+
+        setContentView(R.layout.activity_main);
+        webview =(WebView)findViewById(R.id.webView);
+        spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        webview.setWebViewClient(new CustomWebViewClient());
+
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl(myurl);
+    }
+}
+
